810488795886c65e1dd373c12b3152cad742c86c
Miguel A M <miguelalmont87@gmail.com>
2020-06-10 14:56:48 +0200

    Generated Value cambiado a IDENTITY

--- src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
+++ src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
@@ -19,7 +19,7 @@ public class Usuario implements Serializable{
 	
 	@Id
 	@Column(name="id")
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
 	@NotBlank
