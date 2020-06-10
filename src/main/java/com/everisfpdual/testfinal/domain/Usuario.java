52bb9d5f4e6f8addbb67c4a74338bf65bc4672db
Miguel A M <miguelalmont87@gmail.com>
2020-06-10 09:50:45 +0200

    Entity Usuario terminada

--- src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
+++ src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
@@ -9,14 +9,85 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 @Entity
 @Table(name = "users")
 public class Usuario implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	//Enunciado: Desarrolla la entidad para obtener los datos de BBDD
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
 	
+	@NotBlank
+    @Column(unique=true, name="email")
+	private String email;
 	
+	@NotBlank
+	@Column(name="firstname")
+	private String firstname;
 	
+	@NotBlank
+	@Column(name="lastname")
+	private String lastname;
+	
+	@NotBlank
+	@Column(name="password")
+	private String password;
+	
+	public Usuario() {
+		
+	}
+
+	public Usuario(int id, String email, String firstname, String lastname, String password) {
+		this.firstname = firstname;
+		this.id = id;
+		this.lastname = lastname;
+		this.password = password;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
